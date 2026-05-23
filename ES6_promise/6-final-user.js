95f37908d4fe0a84ec2645e05d2826447f6d9a37
malikzadegulshan <malikzade.gulshan@gmail.com>
2026-05-23 15:21:48 +0400

    Update again

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+
+  return results.map(({ status, value, reason }) => ({
+    status,
+    value: status === 'fulfilled' ? value : reason,
+  }));
 }
