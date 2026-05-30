bcd871bf3322c8f76737e173c8bfe6cc8671e2c4
malikzadegulshan <malikzade.gulshan@gmail.com>
2026-05-31 00:07:47 +0400

    Added file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    });
+
+    Object.entries(fields).forEach(([field, names]) => {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    });
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
