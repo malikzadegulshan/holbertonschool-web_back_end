d9b95eecab4d28a83b4aa4d4b1884f75cc37afa6
malikzadegulshan <malikzade.gulshan@gmail.com>
2026-05-30 23:55:46 +0400

    Update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\n`);
-  rl.close();
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.trim()}\n`);
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
